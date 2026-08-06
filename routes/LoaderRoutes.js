b7bc0181a238cbb19efcfbe91e9a9cf29bc1fd5b
eliy5550 <eliy5550@gmail.com>
2026-08-06 20:44:10 +0400

    Update LoaderRoutes.js

--- routes/LoaderRoutes.js
+++ routes/LoaderRoutes.js
@@ -173,8 +173,8 @@ LoaderRoutes.post("/test0to100" , (req, res)=>{
 })
 
 LoaderRoutes.post("/ai_daily_report" , async (req , res)=>{
-    if (req.date === undefined|| req.date == null ){return res.status(400).json({message : "date not recived"})}
-    const results = await databaseConnection.dbQuery(`select * from daily_ai_review where report_date = "${req.date}";`)
+    if (req.body.date === undefined|| req.date == null ){return res.status(400).json({message : "date not recived"})}
+    const results = await databaseConnection.dbQuery(`select * from daily_ai_review where report_date = "${req.body.date}";`)
     return res.status(200).json(results)
 })
 
