d889a180ca8f18f6ed312f717edcbfa63f069ce3
eliy5550 <eliy5550@gmail.com>
2026-08-11 14:47:46 +0400

    Update LoaderRoutes.js

--- routes/LoaderRoutes.js
+++ routes/LoaderRoutes.js
@@ -183,5 +183,10 @@ LoaderRoutes.post("/all_machines_data" , async (req , res)=>{
     return res.status(200).json(results)
 })
 
+LoaderRoutes.post("/get_all_machines_data_by_type" , async (req, res)=>{
+    const results = await databaseConnection.dbQuery(`select * from machines where machine_type="${req.body.machine_type}";`)
+    return res.status(200).json(results)
+})
+
 
 module.exports = LoaderRoutes
